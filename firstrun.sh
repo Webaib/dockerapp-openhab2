77adcfd04da9cfbd28f037151be204a0c09223b6
christianwaite <christianjwaite@gmail.com>
2016-01-24 13:37:29 +0000

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 if [ ! -d /config/userdata ]; then
 	echo '******MOVING**********'
-	#mv /opt/openhab/conf /config/
-	#mv /opt/openhab/addons /config/
-	#mv /opt/openhab/configurations /config/
-        mv /opt/openhab/userdata /config
+	mv /opt/openhab/conf /config/
+	mv /opt/openhab/addons /config/
+        mv /opt/openhab/userdata /config/
 
 	echo '******LINKING**********'
-	#ln -s /config/conf /opt/openhab/conf
-	#ln -s /config/addons /opt/openhab/addons
-	#ln -s /config/configurations /opt/openhab/configurations
+	ln -s /config/conf /opt/openhab/conf
+	ln -s /config/addons /opt/openhab/addons
         ln -s /config/userdata /opt/openhab/userdata
 
 	chown -R openhab:openhab /config /config/addons /config/conf
